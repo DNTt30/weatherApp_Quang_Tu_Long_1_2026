a3d16999d8a1c5a189ec48228542492a79ab8484
DNTt30 <duongngoctund2004@gmail.com>
2026-04-08 07:50:22 +0700

    Nhiệm vụ 4: Khởi tạo dự án và cập nhật thông tin thành viên nhóm

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Thay đổi Title của App theo yêu cầu
+    return MaterialApp(
+      title: 'Ứng dụng Dự báo Thời tiết', 
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false, // Ẩn chữ debug góc phải
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Dự báo Thời tiết - Nhóm 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'THÔNG TIN THÀNH VIÊN NHÓM',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.blue),
+            ),
+            SizedBox(height: 30),
+            Text(
+              '1. Dương Ngọc Tú - Mã SV: 22010052',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 15),
+            Text(
+              '2. Lê Minh Quang - Mã SV: 21012086',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
